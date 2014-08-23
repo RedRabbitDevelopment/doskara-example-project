59f4031e4c75d878b377364af4bc96e5cc976148
Nathan Tate <nathantate88@gmail.com>
2014-08-23 16:22:54 -0600

    ADDED TO OUTPUT

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 
 var express = require('express');
 var app = express();
-var doskara = require('doskara');
-doskara.init(require('./Doskara.json'));
-var dataStore = new doskara.Client('doskara-example-data-store');
-var transformer = require('./transformer');
+var links = require('docker-links').parseLinks(process.env);
 
-dataStore.init().then(function() {
-  console.log('Setting their before save', dataStore);
-  //dataStore.setBeforeSave(transformer).then(function() {
-    dataStore.save('booya').then(function() {
-      console.log('saved booya');
-   // });
-  }).then(function(e) {
-    console.log('rejected', e);
-  });
-});
+var client = require('socket.io-client')('https://' + links.store.hostname + ':' + links.store.port);
+
 app.use(function(req, res, next) {
   console.log('got req, ' + req.url);
   next();
@@ -32,15 +21,15 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 app.get('/data', function(req, res) {
-  console.log('getting all of their stuff', dataStore, Object.keys(dataStore));
-  dataStore.getAll().then(function(data) {
+  console.log('getting all');
+  client.emit('getAll', function(data) {
     console.log('got all', data);
     res.json(data);
   });
 });
 app.post('/data', function(req, res) {
   console.log('about to save', req.body);
-  dataStore.save(req.body.body).then(function() {
+  client.emit('save', req.body.body, function() {
     console.log('got result');
     res.json({success: true});
   });
